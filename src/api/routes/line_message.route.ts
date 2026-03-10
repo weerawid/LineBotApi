72ac9b5902ed2dab963b2816c3d697dcacc34130
w.nitinunchaiyakul <osacn_weerawid.nitinunchaiyakul@krungthai.com>
2026-03-10 17:43:29 +0700

    fix path

--- src/api/routes/line_message.route.ts
+++ src/api/routes/line_message.route.ts
@@ -4,6 +4,6 @@ import * as controler from '../controller/line_message.controller'
 const router = express.Router();
 
 router.post('/create', controler.create);
-router.put('/:id', controler.update);
+router.put('/update/:id', controler.update);
 
 export default router;
