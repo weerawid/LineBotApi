759e05fb9a9e1ad14fc809900d4db48d7c551597
w.nitinunchaiyakul <osacn_weerawid.nitinunchaiyakul@krungthai.com>
2026-03-10 19:38:46 +0700

    update

--- src/core/dbclient/dbclient.ts
+++ src/core/dbclient/dbclient.ts
@@ -164,7 +164,7 @@ export class DBClientManager {
         throw new AppError(ErrorKey.DB_EXECUTEION_FAILURE_00301,"Values must be a non-empty object");
       }
       const valueFiltered =  Object.fromEntries(
-        Object.entries(values).filter(([_, v]) => v !== undefined && v !== null)
+        Object.entries(values).filter(([_, v]) => v !== undefined)
       )
       if (Object.keys(valueFiltered).length === 0) {
         throw new AppError(ErrorKey.DB_EXECUTEION_FAILURE_00301, "At least one column must be provided for update");
